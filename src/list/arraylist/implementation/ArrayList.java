0b67d2a17f9655fb6427524109d280eb8f7aee35
wnsrb003 <wnsrb003@naver.com>
2020-10-25 15:53:28 +0900

    ArrayList 구현

--- src/list/arraylist/implementation/ArrayList.java
+++ src/list/arraylist/implementation/ArrayList.java
@@ -3,19 +3,73 @@ package list.arraylist.implementation;
 public class ArrayList {
 	private int size=0;
 	private Object[] elementData = new Object[100];
+	
+	public boolean addFirst(int element) {
+		return add(0, element);
+	}
+	
 	public boolean addLast(Object element) {
 		elementData[size] = element;
 		size+=1;
 		return true;
 		
 	}
-	public boolean add(int i, int j) {
+	public boolean add(int index, int element) {
 		
-		
-		elementData[i] = j;
+		for(int i=size-1;i>=index;i--) {
+			elementData[i+1] = elementData[i];
+		}
+		elementData[index] = element;
+		size++;
 		
 		return true;
 	}
 	
+	public String toString() {
+		String str = "[";
+		for(int i =0;i<size;i++) {
+			str += elementData[i];
+			if(i < size-1) {
+				str += ",";
+			}
+		}
+		return str+ "]";
+	}
 
+	public Object get(int index) {
+		
+		return elementData[index];
+	}
+	public int size() {
+		return size;
+	}
+	public int indexOf(Object o) {
+		for(int i=0;i<size;i++) {
+			if(o.equals(elementData[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public ListIterator listIterator() {
+		return new ListIterator();
+	}
+	
+	class ListIterator{
+		private int nextIndex=0;
+		
+		public boolean hasNext() {
+			return nextIndex<size;
+		}
+		public Object next() {
+			return elementData[nextIndex++];
+		}
+		public boolean hasPrevious() {
+			return nextIndex>0;
+		}
+		public Object previous() {
+			return elementData[--nextIndex];
+		}
+	}
+	
 }
--- src/list/arraylist/implementation/Main.java
+++ src/list/arraylist/implementation/Main.java
@@ -8,8 +8,18 @@ public class Main {
 		numbers.addLast(20);
 		numbers.addLast(30);
 		numbers.addLast(40);
-		numbers.add(1,15);
+//		numbers.add(1,15);
+//		numbers.addFirst(5);
+//		System.out.println(numbers);
+//		System.out.println(numbers.get(1));
 		
+		ArrayList.ListIterator li = numbers.listIterator();
+		while(li.hasNext()) {
+			System.out.println(li.next());
+		}
+		while(li.hasPrevious()) {
+			System.out.println(li.previous());
+		}
 	}
 
 }
